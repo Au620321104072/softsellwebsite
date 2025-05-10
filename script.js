8df07aebf53a142028ceb94d317dc8f715fd1f05
Au620321104072 <146078111+Au620321104072@users.noreply.github.com>
2025-05-10 20:01:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtn = document.getElementById("themeToggle");
+    toggleBtn.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+    });
+  });
+  
